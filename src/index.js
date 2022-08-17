b57b2f902eef14592cd838bbca3e159176491902
Nouman Shafi <noumanshafi856@gmail.com>
2022-08-17 12:18:56 +0500

    Change BrowserRouter To HashRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./style.css"
 import { AppProvider } from "./Context";
 ReactDOM.render(
     <AppProvider>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </AppProvider>
 
     ,
